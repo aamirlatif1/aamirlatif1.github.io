35c73c92f6b4638434cf627792458d81d4cdf4bf
Omar Amin <omar4it1@gmail.com>
2025-11-06 00:21:44 +0500

    todo list add duedate

--- js-learning/scripts/todo.js
+++ js-learning/scripts/todo.js
@@ -1,15 +1,54 @@
-const todoList = [];
+const todoList = [{
+    Name: 'Make dinner', 
+    duedate: '2022-10-12'
+    },{
+    Name:'wash dishes',
+duedate: '2022-10-12'}];
 
+renderTodoList();
+function renderTodoList() {
+    
+let todoListHTML = '';
+
+for (let i = 0; i < todoList.length; i++) {
+    const todoobject = todoList[i];
+    /*const name = todoobject.Name;*/
+    /*const duedate = todoobject.duedate;*/
+    const {name , duedate} = todoobject;
+    const html = `
+    <p>
+    ${name} ${duedate} <button onclick="todoList.splice (${i}, 1);
+        renderTodoList();
+    ">Delete</button>
+    </p>
+    `;
+
+    todoListHTML += html;
+
+}
+
+document.querySelector ('.js-todo-list')
+    .innerHTML = todoListHTML;
+}
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
 
    const name = inputElement.value;
+
+   const dateinputElement = document.querySelector('.js-due-date-input')
+   const duedate = dateinputElement.value;
    
-   todoList.push(name);
+   todoList.push({
+    name,
+    duedate,
+    });
+
    console.log(todoList);
 
    inputElement.value = '';
+
+   renderTodoList();
 
    
 
