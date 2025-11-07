377a4d13c4ddf3cc40ff996db8d9c6be8c323cd3
Omar Amin <omar4it1@gmail.com>
2025-11-08 01:32:17 +0500

    code

--- js-learning/scripts/todo.js
+++ js-learning/scripts/todo.js
@@ -1,26 +1,27 @@
 const todoList = [{
     Name: 'Make dinner', 
     duedate: '2022-10-12'
-    },{
-    Name:'wash dishes',
-duedate: '2022-10-12'}];
+    }],
+    [{
+    Name: 'Make dinner', 
+    duedate: '2022-10-12'
+    }]
 
-renderTodoList();
-function renderTodoList() {
+    renderTodoList();
+    function renderTodoList() {
     
-let todoListHTML = '';
-
-for (let i = 0; i < todoList.length; i++) {
+    let todoListHTML = '';
+    for (let i = 0; i < todoList.length; i++) {
     const todoobject = todoList[i];
-    /*const name = todoobject.Name;*/
-    /*const duedate = todoobject.duedate;*/
-    const {name , duedate} = todoobject;
+    const name = todoobject.Name;
+    const duedate = todoobject.duedate;
+    
     const html = `
-    <p>
-    ${name} ${duedate} <button onclick="todoList.splice (${i}, 1);
+    <div>${name}</div>
+     <div>${duedate}</div> 
+     <button onclick="todoList.splice (${i}, 1);
         renderTodoList();
     ">Delete</button>
-    </p>
     `;
 
     todoListHTML += html;
@@ -31,7 +32,7 @@ document.querySelector ('.js-todo-list')
     .innerHTML = todoListHTML;
 }
 
-function addTodo() {
+function addtodo() {
     const inputElement = document.querySelector('.js-name-input');
 
    const name = inputElement.value;
