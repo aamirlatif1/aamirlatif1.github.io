5b44f297adc581ec0eb710dd21ea8ae48a92e6d6
mustafa2647 <aamirmustafabin@gmail.com>
2025-11-07 22:01:47 +0100

    fix display name

--- js-learning/scripts/todo.js
+++ js-learning/scripts/todo.js
@@ -1,55 +1,48 @@
 const todoList = [{
-    Name: 'Make dinner', 
+    Name: 'Make dinner',
     duedate: '2022-10-12'
-    },
-    {
-    Name: 'Make dinner', 
+},
+{
+    Name: 'Make dinner',
     duedate: '2022-10-12'
-    }]
+}]
 
-    renderTodoList();
-    function renderTodoList() {
-    
+renderTodoList();
+function renderTodoList() {
+
     let todoListHTML = '';
     for (let i = 0; i < todoList.length; i++) {
-    const todoobject = todoList[i];
-    const name = todoobject.Name;
-    const duedate = todoobject.duedate;
-    
-    const html = `
-    <div>${name}</div>
-     <div>${duedate}</div> 
-     <button onclick="todoList.splice (${i}, 1);
-        renderTodoList();
-    ">Delete</button>
+        const todoobject = todoList[i];
+        const name = todoobject.Name;
+        const duedate = todoobject.duedate;
+
+        const html = `
+        <div>${name}</div>
+        <div>${duedate}</div> 
+        <button onclick="todoList.splice (${i}, 1);
+        renderTodoList(); ">Delete</button>
     `;
 
-    todoListHTML += html;
+        todoListHTML += html;
+    }
+
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHTML;
 }
 
-document.querySelector ('.js-todo-list') 
-    .innerHTML = todoListHTML;
-}
-
-    function addtodo() {
+function addtodo() {
     const inputElement = document.querySelector('.js-name-input');
 
-   const name = inputElement.value;
+    const name = inputElement.value;
 
-   const dateinputElement = document.querySelector('.js-due-date-input')
-   const duedate = dateinputElement.value;
-   
-   todoList.push({
-    name,
-    duedate,
+    const dateinputElement = document.querySelector('.js-due-date-input')
+    const duedate = dateinputElement.value;
+
+    todoList.push({
+        Name: name,
+        duedate: duedate,
     });
-
-   console.log(todoList);
-
-   inputElement.value = '';
-
-   renderTodoList();
-
-   
-
+    console.log(todoList);
+    inputElement.value = '';
+    renderTodoList();
 }
