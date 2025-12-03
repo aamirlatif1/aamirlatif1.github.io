438221d58f9a47c22f8f55768e1a22abf8666ee4
Omar Amin <omar4it1@gmail.com>
2025-12-04 01:34:09 +0500

    Move orders page to react

--- react-course/vite-project/src/App.jsx
+++ react-course/vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router'
 import { Homepage } from './Pages/Homepage.jsx'
 import { CheckoutPage } from './Pages/CheckoutPage.jsx'
+import { OrdersPage } from './Pages/OrdersPage.jsx'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route index element={<Homepage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrdersPage />} />
       
     </Routes>
   )
