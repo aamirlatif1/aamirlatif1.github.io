a4e64fbb298b1bb529340dba2c800a2996b9e01d
Omar Amin <omar4it1@gmail.com>
2025-12-03 22:06:59 +0500

    Create multiple pages using react-route

--- react-course/vite-project/src/App.jsx
+++ react-course/vite-project/src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from 'react-router'
 import { Homepage } from './Pages/Homepage.jsx'
 import './App.css'
 
 function App() {
   
   return (
-    <Homepage />
+    <Routes>
+      <Route index element={<Homepage />} />
+      <Route path="/checkout" element={<div>Checkout Page</div>} />
+      <Route path="/orders" element={<div>Orders Page</div>} />
+      <Route path="*" element={<div>404 Not Found</div>} />
+      {/* Add more routes as needed */}
+      {/* Example: <Route path="/product/:id" element={<ProductPage />} /> */}
+      {/* Example: <Route path="/about" element={<AboutPage />} /> */}
+    </Routes>
   )
 
       
